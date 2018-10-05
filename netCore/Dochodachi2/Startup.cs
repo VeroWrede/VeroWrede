c88b73b9d89c44e977704757499a7c3494b81690
Vero Wrede <vwrede@decisionhealth.com>
2018-10-05 09:53:41 -0400

    almost finished dachi

--- netCore/Dochodachi2/Startup.cs
+++ netCore/Dochodachi2/Startup.cs
@@ -15,6 +15,8 @@ namespace Dochodachi2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,8 @@ namespace Dochodachi2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseSession();
+            app.UseMvc();
         }
     }
 }
