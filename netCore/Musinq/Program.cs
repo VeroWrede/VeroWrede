54eab0e2b0a3cbd97507b8cc3447dfb8fec77805
Vero Wrede <vwrede@decisionhealth.com>
2018-10-26 10:18:15 -0400

    Music LINQ

--- netCore/Musinq/Program.cs
+++ netCore/Musinq/Program.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using JsonData;
 
-namespace Musinq
+namespace ConsoleApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            Artist theOne = Artists.Where(a => a.Hometown == "Mount Vernon");
+
+            //Who is the youngest artist in our collection of artists?
+            Artist kiddo = Artist.FirstOrDefault(artist => artist.age == Artists.Min(a => a.Age));
+
+            //Display all artists with 'William' somewhere in their real name
+            IEnumerabel<Artist> willies = Artists.Where(a => a.Contains("William"));
+
+            //Display the 3 oldest artist from Atlantap;
+            IEnumerabel<Artist> oldies = Artists.Take(3).OrderBy(a => a.age);
+            IEnumerabel<Artist> oldies = Artists.Take(3).OrderBy(a => a.age).Reverse(); //??
+
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+            
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+	    Console.WriteLine(Groups.Count);
+        }
     }
 }
